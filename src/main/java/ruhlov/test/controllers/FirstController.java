4b62e131c3433bb88f1680c61001d0efb35d3cdf
RukhlovAS <wildac89@mail.ru>
2022-09-08 23:55:44 +0300

    использование thymeleaf для вывода ключей запроса

--- src/main/java/ruhlov/test/controllers/FirstController.java
+++ src/main/java/ruhlov/test/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ruhlov.test.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,8 +10,10 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value="surname", required = false) String surname) {
-        System.out.println("hello " + name + " " + surname);
+                            @RequestParam(value="surname", required = false) String surname,
+                            Model model) {
+        //System.out.println("hello " + name + " " + surname);
+        model.addAttribute("message", "hello " + name + " " + surname);
         return "first/hello";
     }
 
