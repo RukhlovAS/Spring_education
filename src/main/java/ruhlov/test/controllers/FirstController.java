fd6d1b556841382f529fca2e91f9b9f9f91fcbf5
RukhlovAS <wildac89@mail.ru>
2022-09-09 00:10:19 +0300

    простой калькулятор через гет запрос (сложение, вычитание, умножение, деление)

--- src/main/java/ruhlov/test/controllers/FirstController.java
+++ src/main/java/ruhlov/test/controllers/FirstController.java
@@ -9,11 +9,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value="surname", required = false) String surname,
+    public String helloPage(@RequestParam(value = "a", required = false) int a,
+                            @RequestParam(value="b", required = false) int b,
+                            @RequestParam(value="action", required = false) String action,
                             Model model) {
         //System.out.println("hello " + name + " " + surname);
-        model.addAttribute("message", "hello " + name + " " + surname);
+        String result = "";
+        switch(action) {
+            case ("multiplication"):
+                result = a + " * " + b + " = " + (a*b);
+                break;
+            case ("addition"):
+                result = a + " + " + b + " = " + (a+b);
+                break;
+            case ("subtraction"):
+                result = a + " - " + b + " = " + (a-b);
+                break;
+            case ("division"):
+                result = a + " / " + b + " = " + (a/b);
+                break;
+        }
+        model.addAttribute("message", result);
         return "first/hello";
     }
 
