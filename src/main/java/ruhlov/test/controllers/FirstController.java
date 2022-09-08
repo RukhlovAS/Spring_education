2067beb4d9ed597dfe2f3850dd7e294238f55204
RukhlovAS <wildac89@mail.ru>
2022-09-08 23:43:17 +0300

    добавление реквест параметров в гет строку

--- src/main/java/ruhlov/test/controllers/FirstController.java
+++ src/main/java/ruhlov/test/controllers/FirstController.java
@@ -2,12 +2,15 @@ package ruhlov.test.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value="surname", required = false) String surname) {
+        System.out.println("hello " + name + " " + surname);
         return "first/hello";
     }
 
